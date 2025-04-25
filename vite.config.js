f48c09087fa752e92d2d402a30f1863fc73df9f0
Jere Rantalainen <38108067+JRgraphics@users.noreply.github.com>
2025-04-25 13:06:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import { config } from "dotenv";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://crusade-app-backend.vercel.app", // Backend server
+        changeOrigin: true, // Ensure the request appears to come from the frontend server
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: Remove '/api' prefix
+      },
+    },
+  },
   plugins: [react()],
   define: {
     "process.env.GOOGLE_CLIENT_ID": process.env.GOOGLE_CLIENT_ID,
